2585a56eb3ec90898cff8f229c6591fb53b5062a
Jianne Roos <74767359+jianneroos@users.noreply.github.com>
2021-09-22 10:15:19 +0000

    balletjes met kleur

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 class Ball {
-  constructor(x, y, w, h, vx, vy){
+  constructor(x, y, w, h, vx, vy, color){
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.vx = vx;
     this.vy = vy;
+    this.color = color;
   }
 
   drawBall(){
+    fill(this.color);
     ellipse (this.x, this.y, this.w, this.h);
     this.x = this.x + this.vx;
     this.y = this.y + this.vy;
@@ -26,9 +28,10 @@ class Ball {
 function setup() {
   createCanvas (500,400);
   
-  ball1 = new Ball(10,10,50,50,5,5);
-  ball2 = new Ball(50,25,60,60,7,7);
-  ball3 = new Ball(230,60,35,35,3,3)
+  ball1 = new Ball(10,10,50,50,5,5,"pink");
+  ball2 = new Ball(50,25,60,60,7,7,"red");
+  ball3 = new Ball(230,60,35,35,3,3,"orange");
+  ball4 = new Ball(155,120,65,65,6,6,"purple");
 }
 
 function draw() {
@@ -37,6 +40,7 @@ function draw() {
     ball1.drawBall();
     ball2.drawBall();
     ball3.drawBall();
+    ball4.drawBall();
 }
 
 
